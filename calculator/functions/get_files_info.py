47fa3bf65b931ca857752fc677d438f378ac4cf7
Thomas Vu <thomasvu.tech@gmail.com>
2026-07-06 14:31:30 -0700

    Implement file getting logic

---
+++ calculator/functions/get_files_info.py
@@ -0,0 +1,16 @@
+import os
+def get_files_info(working_directory: str, directory: str = ".") -> str:
+    try:
+        direct = os.path.abspath(working_directory)
+        joined = os.path.join(direct, directory)
+        target_dir = os.path.normpath(joined)
+
+        valid_target_dir = os.path.commonpath([direct, target_dir]) == direct
+        if not valid_target_dir:
+            raise Exception(f'Error: Cannot list"{directory}" as it is outside the permitted working directory')
+        if not directory:
+            return(f'Error: "{directory}" is not a directory')
+        else:
+            return f'Success: "{directory}" is within the working directory'
+    except:
+        return "Error: Standard library isn't working"
