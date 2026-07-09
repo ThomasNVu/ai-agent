e78ade7c9dce984364506221bdcefcfac4e4b57d
Thomas Vu <thomasvu.tech@gmail.com>
2026-07-09 16:14:40 -0700

    Implement get_file_info

--- calculator/functions/get_files_info.py
+++ calculator/functions/get_files_info.py
@@ -1,16 +1,31 @@
 import os
 def get_files_info(working_directory: str, directory: str = ".") -> str:
     try:
-        direct = os.path.abspath(working_directory)
-        joined = os.path.join(direct, directory)
-        target_dir = os.path.normpath(joined)
+        direct = os.path.abspath(
+            working_directory
+        )  # Converts directory to absolute for reliability
+        joined = os.path.join(
+            direct, directory
+        )  # Combines working directory with directory to get ex: ../folder/file.py
+        target_dir = os.path.normpath(joined)  # Cleans up path by removing . and ..
 
         valid_target_dir = os.path.commonpath([direct, target_dir]) == direct
-    except:
-        return "Error: Standard library isn't working"
-    if not valid_target_dir:
-        return(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
-    if not os.path.isdir(target_dir):
-        return(f'Error: "{directory}" is not a directory')
-    else:
-        return f'Success: "{directory}" is within the working directory'
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(target_dir):  # Ensures path is a direcotry and not a file
+            return f'Error: "{directory}" is not a directory'
+
+        print(f'Success: "{directory}" is within the working directory')
+        result = []
+        for item in os.listdir(target_dir):
+            item_path = os.path.join(
+                target_dir, item
+            )  # Get's the rest of the items from filepath
+            file_size = os.path.getsize(item_path)  # Check for size
+            is_dir = os.path.isdir(item_path)  # Check if the filepath is there
+            result.append(f"- {item}: file_size={file_size} bytes, is_dir={is_dir}")
+
+        return "\n".join(result)
+
+    except Exception as e:
+        return f"Error listing files: {e}"
