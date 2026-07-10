3c6bf150579f5d9e4858705e2b45117449c08406
Thomas Vu <thomasvu.tech@gmail.com>
2026-07-10 01:16:07 -0700

    Insert System calls from AI

--- main.py
+++ main.py
@@ -2,42 +2,58 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 import argparse
+from prompts import system_prompt
+from call_function import available_functions
+import json
 
 load_dotenv()
 api_key = os.environ.get("OPENROUTER_API_KEY")
-if api_key == None:
+if api_key is None:
     raise RuntimeError("Environment not loaded")
 
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
     api_key=api_key,
 )
+
+
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-    messages=[
+    messages = [
+        {"role": "system", "content": system_prompt},
         {
             "role": "user",
             "content": args.user_prompt,
-        }
+        },
     ]
     if args.verbose:
-        print(f'User prompt: {args.user_prompt}\n')
+        print(f"User prompt: {args.user_prompt}\n")
 
     generate_content(client, messages, args.verbose)
 
 
 def generate_content(client: OpenAI, messages: list, verbose: bool = False) -> None:
-    response = client.chat.completions.create(model='openrouter/free', messages=messages)
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=messages,
+        tools=available_functions,
+    )
+    message = response.choices[0].message
     if not response.usage:
         raise RuntimeError("API response appears to be malformed")
     if verbose:
-        print(f'Prompt tokens: {response.usage.prompt_tokens}')
-        print(f'Response tokens: {response.usage.completion_tokens}')
+        print(f"Prompt tokens: {response.usage.prompt_tokens}")
+        print(f"Response tokens: {response.usage.completion_tokens}")
+    if message.tool_calls:
+        for tool_call in message.tool_calls:
+            function_args = json.loads(tool_call.function.arguments or "{}")
+            print(f"Calling function: {tool_call.function.name}({function_args})")
+    else:
+        print(message.content)
 
-    print(response.choices[0].message.content)
 
 if __name__ == "__main__":
     main()
